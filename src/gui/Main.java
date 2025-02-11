e3746b97eda85914bfc4738ce8f7ca56abf2940c
Jeysson Javier Reino Gelis <jgelis@inforcol.local>
2025-02-11 12:26:04 -0500

    Cambio de Look And Feel

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,9 +1,20 @@
 package gui;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		try {
+			// Establece el Look and Feel del sistema operativo
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
 		Display display = new Display();
 
 	}
